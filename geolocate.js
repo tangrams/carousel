afc3a625b975abfc3ecded298c1aeefba1b8e930
Lou Huang <lou@louhuang.com>
2015-05-11 14:26:21 -0400

    Add geolocate icon. Cache geolocation on page load.

--- geolocate.js
+++ geolocate.js
@@ -1,48 +1,71 @@
-var getCurrentLocation = function (success, error) {
+/* global map */
+(function () {
+  'use strict'
+  var latitude
+  var longitude
+
+  if (!map) return false
+
+  var getCurrentLocation = function (success, error) {
     var geolocator = window.navigator.geolocation;
     var options = {
-        enableHighAccuracy: true,
-        maximumAge: 10000
+      enableHighAccuracy: true,
+      maximumAge: 10000
     };
 
     if (geolocator) {
-        // Fixes an infinite loop bug with Safari
-        // https://stackoverflow.com/questions/27150465/geolocation-api-in-safari-8-and-7-1-keeps-asking-permission/28436277#28436277
-        window.setTimeout(function () {
-          geolocator.getCurrentPosition(success, error, options);
-        }, 0);
+      // Fixes an infinite loop bug with Safari
+      // https://stackoverflow.com/questions/27150465/geolocation-api-in-safari-8-and-7-1-keeps-asking-permission/28436277#28436277
+      window.setTimeout(function () {
+        geolocator.getCurrentPosition(success, error, options);
+      }, 0);
     } else {
-        document.getElementById('geolocator').style.display = 'none';
-        console.log('Browser does not support geolocation');
+      document.getElementById('geolocator').style.display = 'none';
+      console.log('Browser does not support geolocation');
     }
-}
+  }
 
-var onGeolocateSuccess = function (position) {
+  var cacheCurrentLocation = function () {
+    var onSuccess = function (position) {
+    latitude = position.coords.latitude
+    longitude = position.coords.longitude
+    }
+
+    // Do nothing if we are unable to do geolocation
+    // No error callback
+
+    getCurrentLocation(onSuccess)
+  }
+
+  var onGeolocateSuccess = function (position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
 
     /* global map */
     map.setView([latitude, longitude], 17);
     resetGeolocateButton();
-}
+  }
 
-var onGeolocateError = function (err) {
+  var onGeolocateError = function (err) {
     console.log(err);
     alert('Unable to retrieve current position. Geolocation may be disabled on this browser or unavailable on this system.');
     resetGeolocateButton();
-}
+  }
 
-var resetGeolocateButton = function () {
-    var button = document.getElementById('geolocator').querySelector('.geolocate-button');
+  var resetGeolocateButton = function () {
+    var button = document.getElementById('geolocator').querySelector('.geolocate-icon');
     button.classList.remove('geolocating');
-    button.textContent = '🌐'
-}
+  }
 
-document.getElementById('geolocator').querySelector('.geolocate-button').addEventListener('click', function (e) {
+  document.getElementById('geolocator').querySelector('.geolocate-icon').addEventListener('click', function (e) {
     if (e.target.classList.contains('geolocating')) {
-        return false;
+      return false;
     }
-    e.target.textContent = '!';
     e.target.classList.add('geolocating');
     getCurrentLocation(onGeolocateSuccess, onGeolocateError);
-});
+  });
+
+  // Requests browser's permission to use
+  // geolocator upon page load, if necessary
+  cacheCurrentLocation()
+})()
