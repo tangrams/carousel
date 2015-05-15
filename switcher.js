57ffd0b2a159367f3b74d825eb65e0010342003f
hanbyul-here <hanbyul@nyu.edu>
2015-05-15 17:20:28 -0400

    added style switcher

---
+++ switcher.js
@@ -0,0 +1,42 @@
+function Switcher(func, params){
+  if (window.self == window.top) {
+    var eventFunction = func;
+    var eventParams = params;
+    var width = 0;
+
+    var switcherEL = document.createElement('div');
+    switcherEL.className = "control";
+    var styleUL = document.createElement('ul');
+
+    var titleLI = document.createElement('li');
+    var titleTxt = document.createTextNode('styles');
+    titleLI.appendChild(titleTxt);
+    titleLI.className = 'title';
+    styleUL.appendChild(titleLI);
+    
+    titleLI.addEventListener('click',function(e){
+
+        var style = document.querySelectorAll('li.style');
+        var len = style.length;
+        var i =0;
+        for( i =0; i<len; i++){
+          console.log("hew");
+          style[i].classList.toggle("show");
+        }
+    });
+
+    params.forEach(function(styleName,index){
+
+      var styleLI = document.createElement('li');
+      var styleTxt = document.createTextNode(styleName);
+      styleLI.appendChild(styleTxt);
+      styleLI.className = 'style';
+      styleLI.addEventListener('click',function(e){
+        func(styleName);
+      });
+      styleUL.appendChild(styleLI);
+    });
+    switcherEL.appendChild(styleUL);
+    document.body.appendChild(switcherEL);
+   }
+}
