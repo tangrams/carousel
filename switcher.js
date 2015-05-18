31573ec5f2aaf912f8c28c32a55828155d5f69f3
hanbyul-here <hanbyul@nyu.edu>
2015-05-18 17:48:48 -0400

    clean up

--- switcher.js
+++ switcher.js
@@ -13,6 +13,7 @@ function Switcher(func, params,cStyle){
     titleLI.appendChild(titleTxt);
     titleLI.className = 'title';
     styleUL.appendChild(titleLI);
+    console.log(titleLI.offsetBottom);
     
     titleLI.addEventListener('click',function(e){
       titleLI.classList.toggle('active');
